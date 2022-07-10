0c879187d01bdec6e23b909cf8e05b5c55644f9a
AndrewRPorter <pavelow53.andrew@gmail.com>
2022-07-10 16:12:36 -0400

    Add scroll to top button

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
+import { useEffect, useState, useRef } from 'react'
 import { AppProps } from 'next/app'
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme, Button } from '@chakra-ui/react'
+import { ChevronUpIcon } from '@chakra-ui/icons'
 
 const config = {
   initialColorMode: 'light',
@@ -7,8 +9,39 @@ const config = {
 }
 
 export default function Application({ Component, pageProps }: AppProps) {
+  const [showScrollToTop, setShowScrollToTop] = useState(false)
+  const refScrollUp = useRef<HTMLDivElement>()
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleVisibleButton)
+  })
+
+  const handleVisibleButton = () => {
+    const position = window.pageYOffset
+    if (position > 100) {
+      return setShowScrollToTop(true)
+    } else if (position < 100) {
+      return setShowScrollToTop(false)
+    }
+  }
+
+  const handleScrollUp = () => {
+    refScrollUp.current.scrollIntoView({ behavior: 'smooth' })
+  }
+
   return (
     <ChakraProvider theme={extendTheme(config)}>
+      <div ref={refScrollUp}></div>
+      {showScrollToTop && (
+        <Button
+          position="fixed"
+          bottom="8%"
+          left="92%"
+          onClick={handleScrollUp}
+        >
+          <ChevronUpIcon />
+        </Button>
+      )}
       <Component {...pageProps} />
     </ChakraProvider>
   )
