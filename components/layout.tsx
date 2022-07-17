9b78c49c165f16d9198c7a7ad74218308422ef71
AndrewRPorter <pavelow53.andrew@gmail.com>
2022-07-17 09:30:58 -0400

    Add meta data

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,16 +7,22 @@ type Props = {
   children: React.ReactNode
   seoTitle?: string
   description?: string
+  ogImage?: string
 }
 
-const DEFAULT_SEO_TITLE = 'Andrew Porter'
-const DEFAULT_SEO_DESCRIPTION = "Andrew Porter's personal website."
+const DEFAULT_SEO_TITLE = 'Andrew Porter - Software Engineer'
+const DEFAULT_SEO_DESCRIPTION = "Andrew Porter's personal website and blog."
+const DEFAULT_OG_IMAGE = '/programmer_icon.png'
 
 export default function Layout(props: Props) {
   return (
     <>
       <Head>
         <meta charSet="utf-8"></meta>
+        <meta
+          property="og:image"
+          content={props.ogImage ? props.ogImage : DEFAULT_OG_IMAGE}
+        />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=5"
@@ -27,6 +33,10 @@ export default function Layout(props: Props) {
             props.description ? props.description : DEFAULT_SEO_DESCRIPTION
           }
         ></meta>
+        <meta
+          property="og:title"
+          content={props.seoTitle ? props.seoTitle : DEFAULT_SEO_TITLE}
+        />
         <title>{props.seoTitle ? props.seoTitle : DEFAULT_SEO_TITLE}</title>
       </Head>
       <Box>
