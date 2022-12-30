bf90a33752cc26726a5862f09214c8d20fd65295
AndrewRPorter <porter.r.andrew@gmail.com>
2022-12-30 12:51:54 -0500

    Add vercel analytics package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { useRef } from 'react'
 import dynamic from 'next/dynamic'
 import { AppProps } from 'next/app'
-import Script from 'next/script'
+import { Analytics } from '@vercel/analytics/react'
 import { ChakraProvider, extendTheme, type ThemeConfig } from '@chakra-ui/react'
 
 const TopOfPageButton = dynamic(() => import('@/components/top_of_page_button'))
@@ -20,24 +20,7 @@ export default function Application({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `
-        }}
-      />
+      <Analytics />
       <ChakraProvider theme={extendTheme({ config })}>
         {/* TODO: look into providing a custom hook to fix this type error: https://stackoverflow.com/a/64151312/8168077.
         This is why we case the type here. */}
