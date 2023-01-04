d6a69c7928e2f4bb49ff4f69705ebb8c6219c400
AndrewRPorter <porter.r.andrew@gmail.com>
2023-01-03 22:16:40 -0500

    Add next.js bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
   i18n: {
     locales: ['en-US'],
@@ -14,3 +14,9 @@ module.exports = {
     ]
   }
 }
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+
+module.exports = withBundleAnalyzer(nextConfig)
